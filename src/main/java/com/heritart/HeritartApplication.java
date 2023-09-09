f6a670f24e283389200a8f8cac10fad8b2efba57
cruelsuerte <vansmilton@gmail.com>
2023-09-09 14:42:20 +0200

    corretta timezone

--- src/main/java/com/heritart/HeritartApplication.java
+++ src/main/java/com/heritart/HeritartApplication.java
@@ -4,12 +4,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 public class HeritartApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeritartApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Rome"));
 	}
 
 	@Override
